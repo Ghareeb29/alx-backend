83a8c60f3dc8e7f9480e0ada0735667c337da3b6
Ghareeb29 <sonic_unleashed@live.com>
2024-08-24 13:14:16 +0300

    Revert "lifo_cache fix 1"
    This reverts commit 19ea3d0b7fe74bcceddfebe74f4d0c017267cb91.

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,41 +12,38 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Add an item to the cache using LIFO algorithm.
-
-        If the cache is at capacity, remove the most recently added item
-        before adding the new item. If key or item is None, this method
-        should not do anything.
+        Adds an item to the cache with the given key,
+        replacing the existing value if it already exists.
 
         Args:
-        key: The key to store the item under.
-        item: The item to be stored in the cache.
+            key (Any): The key to associate the item with.
+            item (Any): The item to store in the cache.
+
+        Returns:
+            None
+
+        Prints:
+            - "DISCARD: {last}" if the cache is full and
+            an item needs to be discarded to make space for the new item.
         """
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            if key not in self.cache_data:
-                discarded = self.order.pop()
-                del self.cache_data[discarded]
-                print(f"DISCARD: {discarded}")
-
-        if key in self.cache_data:
-            self.order.remove(key)
-
-        self.order.append(key)
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                Last = next(reversed(self.cache_data))
+                del self.cache_data[Last]
+                print("DISCARD: {}".format(Last))
 
     def get(self, key):
         """
-        Retrieve an item from the cache.
+        Retrieves an item from the cache based on the provided key.
 
         Args:
-        key: The key of the item to retrieve.
+            key (Any): The key used to identify the item in the cache.
 
         Returns:
-        The value associated with the key if it exists, None otherwise.
+            Any or None: The item associated with the key
+            if it exists in the cache, otherwise None.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
