d14b966eff93f1938ca53427627357e8b0e118fa
Ghareeb29 <sonic_unleashed@live.com>
2024-08-29 02:48:09 +0300

    refix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -9,35 +9,41 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 from typing import Any, Union
 
+
 class Config:
     """Configuration class for Flask app."""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> Union[str, None]:
     """
     Determine the best match for supported languages.
-    
+
     Returns:
         str: Best matching language code.
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-@app.route('/')
+
+@app.route("/")
 def index() -> Any:
     """
     Render the index page with translated content.
-    
+
     Returns:
         str: Rendered HTML content of the index page.
     """
-    return render_template('3-index.html')
+    return render_template("3-index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
