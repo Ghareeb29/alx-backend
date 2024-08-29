fa847cdccd609c6d9aca7fea1844dc9c719ec0b4
Ghareeb29 <sonic_unleashed@live.com>
2024-08-29 03:39:44 +0300

    Task 4 + Task 3 fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,11 +21,11 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale() -> Union[str, None]:
     """Determine the best match for supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route("/")
 def index() -> Any:
